1061a6ba12b04473ce865988c7ed900a1e8d402d
Rina Inada <indrn@icloud.com>
2023-06-19 18:44:18 +0000

    add comments

--- iss.js
+++ iss.js
@@ -12,12 +12,12 @@ const fetchMyIP = (callback) => {
   const api = "https://api.ipify.org?format=json";
 
   request(api, (error, response, body) => {
-    
     // error can be set if invalid domain, user is offline, etc.
     if (error) {
       callback(error, null);
       return;
     }
+
     // if non-200 status, assume server error
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
@@ -25,6 +25,7 @@ const fetchMyIP = (callback) => {
       return;
     }
 
+    // and finally pass the ip to print 
     const ip = JSON.parse(body).ip;
     callback(null, ip);
   });
