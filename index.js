0ff1f19bf579622c5563d00c7446c52004b15d10
Rina Inada <indrn@icloud.com>
2023-07-25 05:40:28 +0000

    Implement promise

--- index.js
+++ index.js
@@ -1,42 +1,5 @@
-// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
-
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
-//   console.log("It worked! Returned IP:", ip);
-// });
-
-// fetchCoordsByIP("162.245.144.188", (error, coordinates) => {
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
-//   console.log("It worked! Returned coordinates:", coordinates);
-// });
-
-// const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
-
-// fetchISSFlyOverTimes(exampleCoords, (error, flyOverTimes) => {
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
-//   console.log("It worked!:", flyOverTimes);
-// });
-
 const { nextISSTimesForMyLocation } = require("./iss");
-
-const printPassTimes = function(passTimes) {
-  console.log("passTimes:", passTimes);
-  for (const pass of passTimes) {
-    const datetime = new Date(0);
-    datetime.setUTCSeconds(pass.risetime);
-    const duration = pass.duration;
-    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
-  }
-};
+const { printPassTimes } = require("./printPassTimes");
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
